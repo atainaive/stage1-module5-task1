605d66e2c8f3e47603ee6aacb6434f9739e443d5
atainaive <atai.omorov@stud.vgtu.lt>
2022-11-29 12:23:41 +0200

    done

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,5 +1,6 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
@@ -7,26 +8,43 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
 
 public class InterfaceCreator {
 
     public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> {
+            return x.stream().allMatch(s -> Character.isUpperCase(s.toCharArray()[0]));
+        };
+
     }
 
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> {
+            x.addAll(x.stream().filter(n -> n % 2 == 0).collect(Collectors.toList()));
+        };
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return () -> {
+           return values.stream().filter(s -> Character.isUpperCase(s.charAt(0)))
+                   .filter(s -> s.endsWith("."))
+                   .filter(s -> s.split(" ").length > 3)
+                   .collect(Collectors.toList());
+        };
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> {
+            return x.stream().collect(toMap(String::toString, String::length));
+        };
     }
 
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (list1, list2) -> {return Stream.concat(list1.stream(), list2.stream()).toList(); };
     }
 }
+
